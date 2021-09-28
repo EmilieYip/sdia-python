46d3141e034917b714361f05835ac7838f514d1b
EmilieYip <yip.emilie@gmail.com>
2021-09-28 08:10:32 +0200

    Fin TP1

--- src/lab1/functions.py
+++ src/lab1/functions.py
@@ -12,3 +12,7 @@ def triangle_shape(height):
     if height == 0:
         return str()
     else :
+        for i in range (height):
+            esp = height-1-i
+            mot = esp*" " + (2*i+1)*"x"
+            print(mot)
