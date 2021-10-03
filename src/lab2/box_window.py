413d8c9f535f332634e7f5355ce310d58a88a792
EmilieYip <yip.emilie@gmail.com>
2021-10-03 11:32:58 +0200

    Changement

--- src/lab2/box_window.py
+++ src/lab2/box_window.py
@@ -1,29 +1,29 @@
-from lab2.utils import get_random_number_generator
+#from lab2.utils import get_random_number_generator
 
 
 class BoxWindow:
     """[summary]"""
 
     def __init__(self, args):
-        """[summary]
+        """initialize the BoxWindow with the bounding points
 
         Args:
-            args ([type]): [description]
+            args (np.array([integer])): array of the bounding points of the box
         """
-        self.bounds = None
+        self.bounds = args
 
-    def __str__(self):
+    def __str__(self): #manière de print l'objet
         r"""BoxWindow: :math:`[a_1, b_1] \times [a_2, b_2] \times \cdots`
 
         Returns:
             [type]: [description]
         """
-        return ""
+        return ("BoxWindow: " + str(self.bounds))
 
-    def __len__(self):
+    def __len__(self): #dimension, faire dependre la fonction dimension et len
         return
 
-    def __contains__(self, args):
+    def __contains__(self, args): #si x est ds la boite/utiliser  dans indicator_function?
         return True or False
 
     def dimension(self):
@@ -62,3 +62,7 @@ class UnitBoxWindow(BoxWindow):
             center ([type], optional): [description]. Defaults to None.
         """
         super(BoxWindow, self).__init__(args)
+
+
+c = BoxWindow(np.array([[2.5, 2.5]]))
+c.bounds
