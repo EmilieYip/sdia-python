3e3bb5000a0ca9529647e719d5fdc451dc0656b2
EmilieYip <yip.emilie@gmail.com>
2021-10-03 12:02:17 +0200

    Tentative TP2

--- src/lab2/box_window.py
+++ src/lab2/box_window.py
@@ -1,46 +1,79 @@
-#from lab2.utils import get_random_number_generator
+import numpy as np
+
+from lab2.utils import get_random_number_generator
 
 
 class BoxWindow:
-    """[summary]"""
+    """Creates a window of rectangular shape"""
 
     def __init__(self, args):
-        """initialize the BoxWindow with the bounding points
+        """initialize the box window with the bounding points
 
         Args:
             args (np.array([integer])): array of the bounding points of the box
         """
         self.bounds = args
 
-    def __str__(self): #manière de print l'objet
+    def __str__(self):
         r"""BoxWindow: :math:`[a_1, b_1] \times [a_2, b_2] \times \cdots`
 
         Returns:
-            [type]: [description]
+            str : give the bounds of the box
         """
-        return ("BoxWindow: " + str(self.bounds))
+        mot = ""
+        for [a, b] in self.bounds:
+            mot = mot + str([a, b]) + " x "
+        return "BoxWindow: " + mot[:-3]
 
-    def __len__(self): #dimension, faire dependre la fonction dimension et len
-        return
+    def __len__(self):
+        """
+        Returns:
+            list : list of the length in each direction
+        """
+        L = []
+        for (a, b) in self.bounds:
+            L.append(b - a)
+        return L
 
-    def __contains__(self, args): #si x est ds la boite/utiliser  dans indicator_function?
-        return True or False
-
-    def dimension(self):
-        """[summary]"""
-        return
-
-    def volume(self):
-        """[summary]"""
-        return
-
-    def indicator_function(self, args):
-        """[summary]
+    def __contains__(self, point):
+        """Indicates if the point is contained in the box window.
+        Returns True if the point is in the box, returns False otherwise.
 
         Args:
-            args ([type]): [description]
+            point ([np.array([Integer])): coordonnées d'un point
         """
-        return
+        for (a, b), x in zip(self.bounds, point):
+            if not (a <= x <= b):
+                return False
+        return True
+
+    def dimension(self):
+        """
+        Returns:
+            integer : dimension of the box
+        """
+        return len(self.bounds)
+
+    def volume(self):
+        """
+        Returns:
+            integer : volume of the box
+        """
+        vol = 1
+        for p in self.__len__():
+            vol = vol * p
+        return vol
+
+    def indicator_function(self, args):
+        """Indicator function of the box window. Returns 1 if the point is in the box, returns 0 otherwise.
+
+        Args:
+            args ([np.array([Integer])): coordonnées d'un point
+        """
+        if self.__contains__(args) == True:
+            return 1
+        else:
+            return 0
 
     def rand(self, n=1, rng=None):
         """Generate ``n`` points uniformly at random inside the :py:class:`BoxWindow`.
@@ -50,9 +83,20 @@ class BoxWindow:
             rng ([type], optional): [description]. Defaults to None.
         """
         rng = get_random_number_generator(rng)
-        return
+
+        L = []
+        for p in range(n):
+            L_petit = []
+            for (a, b) in self.bounds:
+                if a == b:
+                    L_petit.append(a)
+                else:
+                    L_petit.append(np.random.uniform(b - a) + a)
+            L.append(L_petit)
+        return L
 
 
+# heritage
 class UnitBoxWindow(BoxWindow):
     def __init__(self, center, dimension):
         """[summary]
@@ -62,7 +106,3 @@ class UnitBoxWindow(BoxWindow):
             center ([type], optional): [description]. Defaults to None.
         """
         super(BoxWindow, self).__init__(args)
-
-
-c = BoxWindow(np.array([[2.5, 2.5]]))
-c.bounds
